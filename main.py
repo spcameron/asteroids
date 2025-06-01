e5733699a4db770ba586f0dd1113da0ae6f5146a
spcameron <cameron.seanpatrick@gmail.com>
2025-06-01 05:50:52 -0400

    Implement splitting asteroids when shot

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def main():
             for shot in shots:
                 if shot.check_collision(asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
             
         screen.fill((0,0,0))
         
